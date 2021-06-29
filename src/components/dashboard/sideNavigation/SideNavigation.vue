670a2cb18c18006218e92943163d97612a6d2714
emperorsegxy <emperorsegxy@github.com>
2021-06-29 20:47:38 +0100

    side nav update

--- src/components/dashboard/sideNavigation/SideNavigation.vue
+++ src/components/dashboard/sideNavigation/SideNavigation.vue
@@ -8,16 +8,24 @@
         <v-sheet>
           <v-tabs slider-color="#F2F2F2" vertical>
             <v-tab v-for="(nav, i) in navs" :key="i" class="pl-2 pl-md-10" :to="nav.to" active-class="tab__active">
-              <v-row class="d-flex align-center nav__name text-capitalize">
-                <v-col cols="2" class="pr-0">
+<!--              <v-row class="d-flex flex-nowrap align-center nav__name text-capitalize">-->
+<!--                <v-col cols="2" class="pr-0">-->
+<!--                  <icon :type="nav.icon" :active="nav.active"/>-->
+<!--                </v-col>-->
+<!--                <v-col cols="9" class="pl-0">-->
+<!--                <span>-->
+<!--                  {{ nav.name }}-->
+<!--                </span>-->
+<!--                </v-col>-->
+<!--              </v-row>-->
+              <div class="d-flex align-center px-4" style="width: 100%;">
+                <div class="mr-3">
                   <icon :type="nav.icon" :active="nav.active"/>
-                </v-col>
-                <v-col cols="9" class="pl-0">
-                <span>
-                  {{ nav.name }}
-                </span>
-                </v-col>
-              </v-row>
+                </div>
+                <div>
+                  {{nav.name}}
+                </div>
+              </div>
             </v-tab>
           </v-tabs>
         </v-sheet>
@@ -30,7 +38,7 @@
 
 import Icon from "@/components/icons/Icon";
 import brand_logo from "@/assets/brand_logo.svg"
-import {mapMutations} from "vuex";
+import {mapMutations, mapState} from "vuex";
 
 export default {
   name: "SideNavigation",
@@ -72,22 +80,23 @@ export default {
     ]
   }),
   computed: {
-    // ...mapState('globals', ['drawer']),
+    ...mapState('globals', [{storeDrawer: 'drawer'}]),
     navs() {
       return this.user_navs
     },
-    // nav_drawer: {
-    //   get() {
-    //     return this.drawer
-    //   },
-    //   set() {
-    //     this.SET_DRAWER()
-    //   }
-    // }
+    smAndDown() {
+      return this.$vuetify.breakpoint.smAndDown
+    }
   },
   watch: {
-    drawer() {
-      this.SET_DRAWER()
+    drawer(value) {
+      this.SET_DRAWER(value)
+    },
+    smAndDown(value) {
+      this.SET_DRAWER(value)
+    },
+    storeDrawer(value) {
+      this.drawer = value
     }
   },
   methods: {
